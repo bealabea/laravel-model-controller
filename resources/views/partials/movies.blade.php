d1c722b2ac3b93101d68f161c22940d8fb526216
bealabea <beatricemarin89@yahoo.it>
2022-03-04 18:31:25 +0100

    Adds movies stars with for and if

--- resources/views/partials/movies.blade.php
+++ resources/views/partials/movies.blade.php
@@ -6,6 +6,13 @@
         <h4 class="text">"{{$movie->original_title}}"</h4>
         <p class="text">"{{$movie->date}}"</p>
         <p class="text"><i>{{$movie->nationality}}</i></p>
+        @for($i =0; $i <5; $i++)
+            @if ($i < floor($movie['vote'] / 2))    
+            <i class="star fas fa-star"></i>
+            @else
+            <i class="star far fa-star"></i>
+            @endif
+        @endfor
     </div>
     @endforeach
 </div>
